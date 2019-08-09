725905815ac9dd61200971c67854ebad3425d165
antoiovi <antoiovi@antoiovi.com>
2019-08-09 17:13:31 +0200

    Deleted old Talk_zip_tar.sh

--- Talk_zip_tar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp Talk.sh target/Talk.sh
-cp Talk.bat target/Talk.bat
-cd target/
-
-tar -cvf Talk-1.2.tar Talk-1.2.jar Talk.sh lib/jssc-2.8.0.jar 
-tar -cvf Talk-1.2.zip Talk-1.2.jar Talk.bat lib/jssc-2.8.0.jar 
